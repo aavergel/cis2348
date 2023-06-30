163d458d6b4fd5e239c7f7406d8c8d50ef5199b1
aavergel <135925276+aavergel@users.noreply.github.com>
2023-06-30 18:35:25 -0500

    output dates into 'parsedDates.txt' - part c

--- homework2/coding problem 2.py
+++ homework2/coding problem 2.py
@@ -4,14 +4,17 @@ tday = datetime.datetime.today()
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
           'December']
 
-with open('inputDates.txt', 'r') as input_dates:
-    for line in input_dates:
-        if line == '-1':
-            break
-        else:
-            for m in months:
-                if line.find(m) != -1:
-                    input_date = datetime.datetime.strptime(line.strip(), '%B %d, %Y')
-                    if input_date < tday:
-                        print(input_date.strftime('%#m/%#d/%Y'))
+input_dates = open('inputDates.txt', 'r')
+output_dates = open('parsedDates.txt', 'w')
+for line in input_dates:
+    if line == '-1':
+        break
+    else:
+        for m in months:
+            if line.find(m) != -1:
+                input_date = datetime.datetime.strptime(line.strip(), '%B %d, %Y')
+                if input_date < tday:
+                    output_dates.write(input_date.strftime('%#m/%#d/%Y\n'))
 
+output_dates.close()
+input_dates.close()
