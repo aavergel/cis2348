0796510d91c8b7b5586951890d24cd23088139c4
aavergel <135925276+aavergel@users.noreply.github.com>
2023-06-29 22:38:35 -0500

    modified to read dates from an input file - part b

--- homework2/coding problem 2.py
+++ homework2/coding problem 2.py
@@ -3,13 +3,15 @@ import datetime
 tday = datetime.datetime.today()
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
           'December']
-while True:
-    user_input = input()
-    if user_input == '-1':
-        break
-    else:
-        for m in months:
-            if user_input.find(m) != -1:
-                input_date = datetime.datetime.strptime(user_input, '%B %d, %Y')
-                if input_date < tday:
-                    print(input_date.strftime('%#m/%#d/%Y'))
+
+with open('inputDates.txt', 'r') as input_dates:
+    for line in input_dates:
+        if line == '-1':
+            break
+        else:
+            for m in months:
+                if line.find(m) != -1:
+                    input_date = datetime.datetime.strptime(line.strip(), '%B %d, %Y')
+                    if input_date < tday:
+                        print(input_date.strftime('%#m/%#d/%Y'))
+
